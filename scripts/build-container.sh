65bdb154a8c91eb0937e68c0a834310a900763cc
Henrik Høegh <henrikhoegh@henriks-mbp.lan>
2023-06-10 16:05:51 +0200

    Added org, image and tag to container build

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -1,1 +1,8 @@
-docker build -t hoeghh/go-test --file .plan/container/Dockerfile .
+export IMAGE_TAG="${IMGTAG:-"latest"}"
+
+if [ -f "bura.yaml" ]; then
+    export IMAGE=$(cat bura.yaml | yq .vars.service.name)
+    export REGISTRY_ORG=$(cat bura.yaml | yq .vars.service.registry.org)
+fi
+
+docker build -t $REGISTRY_ORG/$IMAGE:$IMAGE_TAG --file .plan/container/Dockerfile .
