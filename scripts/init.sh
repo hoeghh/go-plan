a5a36c56c7bfa01c399db2a04dd18d7edd140991
Henrik Høegh <henrikhoegh@henriks-mbp.lan>
2023-06-27 18:53:27 +0200

    only export vars section to env

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VARS=$(cat bura.yaml | yq e '.. | select(. == "*") | {(path | . as $x | (.[] | select((. | tag) == "!!int") |= (["[", ., "]"] | join(""))) | $x | join(".") | sub(".\[", "[")): .} ' -  | sed "s/: /=/g" | sed "s/\./_/g")
+VARS=$(cat bura.yaml | yq e '.vars | .. | select(. == "*") | {(path | . as $x | (.[] | select((. | tag) == "!!int") |= (["[", ., "]"] | join(""))) | $x | join(".") | sub(".\[", "[")): .} ' -  | sed "s/: /=/g" | sed "s/\./_/g")
 echo "- Exporting vars"
 while IFS= read -r line; do
     eval "export $line"
