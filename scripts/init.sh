f911cbec2f16d95961985313776853a418e0d54b
Henrik Høegh <henrikhoegh@henriks-mbp.lan>
2023-06-28 14:40:57 +0200

    added plan and fix in init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ -f "bura.yaml" ]; then
-    VARS=$(cat bura.yaml | yq e '.vars | .. | select(. == "*") | {(path | . as $x | (.[] | select((. | tag) == "!!int") |= (["[", ., "]"] | join(""))) | $x | join(".") | sub(".\[", "[")): .} ' -  | sed "s/: /=/g" | sed "s/\./_/g")
+    VARS=$(cat bura.yaml | yq e '.vars | .. | select(. == "*") | {(path | . as $x | (.[] | select((. | tag) == "!!int") |= (["[", ., "]"] | join(""))) | $x | join("_") | sub(".\[", "[")): .} ' -  | sed "s/: /=/g")
     echo "- Exporting vars"
     while IFS= read -r line; do
         eval "export $line"
+        echo "export $line"
     done <<< "$VARS"
 else
   echo "bura.yaml not found"
